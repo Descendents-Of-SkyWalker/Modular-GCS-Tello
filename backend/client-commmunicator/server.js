d65978cd8d0178d9d2e220e3400360c12b98cea8
Gaurav Singh <gp5in9h@gmail.com>
2021-09-08 09:33:34 +0530

    some stuff done

---
+++ backend/client-commmunicator/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+// middleware setup
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: true}));
+
+let file_no = 0;
+
+// routes
+app.post('/videoFrame', (req, res) => {
+    fs.writeFileSync(`sample_end${file_no}.jpeg`, req.body.frame, 'base64');
+    // console.log('\n');
+    file_no++;
+    res.status(200).json({
+        status: 'success'
+    });
+});
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`running at port ${port}`);
+});
