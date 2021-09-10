a017a940890d6e14c90b0300a21ab18567700c20
Gaurav Singh <gp5in9h@gmail.com>
2021-09-10 11:51:28 +0530

    drone engine drone communication established

--- backend/client-commmunicator/server.js
+++ backend/client-commmunicator/server.js
@@ -7,11 +7,9 @@ const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended: true}));
 
-let file_no = 0;
 
 // routes
 app.post('/videoFrame', (req, res) => {
-    file_no++;
     res.status(200).json({
         status: 'success'
     });
