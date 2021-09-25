6f9de321a4fd9ced41f90125f3a7e017a4610790
Rajvi Jasani <rajvijasani1@gmail.com>
2021-09-26 00:52:19 +0530

    dashboard blueprint ready

--- frontend/main/script.js
+++ frontend/main/script.js
@@ -1,66 +1,56 @@
 const bar = document.querySelector('.bar');
 const links = document.querySelectorAll('a[href^="#"]');
-const sections=document.querySelectorAll('section');
+const sections = document.querySelectorAll('section');
+const navCoords = document.querySelector('nav').getBoundingClientRect();
+const startCoords = document.querySelector('#icon-video').getBoundingClientRect();
+setBounds(startCoords);
 
-links.forEach(link =>{
-    link.addEventListener('click',(e)=>{
+function setBounds(coords) {
+    bar.style.top = `${coords.y}px`;
+    bar.style.height = `${coords.height}px`;
+    bar.style.left = `${navCoords.width - 4}px`;
+}
+
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
         e.preventDefault();
         document.querySelector(`${link.hash}`).scrollIntoView({
-            behavior:"smooth"
+            behavior: "smooth"
         });
-        document.querySelector('#icon-video').src=`icon_video.svg`;
-        document.querySelector('#icon-stats').src=`icon_stats.svg`;
-        document.querySelector(`#icon-${link.hash.substring(1,)}`).src=`icon_${link.hash.substring(1,)}_focused.svg`;
+        document.querySelector('#icon-video').src = `icon_video.svg`;
+        document.querySelector('#icon-stats').src = `icon_stats.svg`;
+        const activeAnchor = document.querySelector(`#icon-${link.hash.substring(1,)}`);
+        activeAnchor.src = `icon_${link.hash.substring(1,)}_focused.svg`;
+        const coords = activeAnchor.getBoundingClientRect();
+        setBounds(coords);
     });
 });
 const options = {
     threshold: 0.7
 };
-let observer = new IntersectionObserver(navCheck,options);
+let observer = new IntersectionObserver(navCheck, options);
 
-function navCheck(entries){
-    entries.forEach(entry =>{
-        const className=entry.target.className;
-        const activeAnchor=document.querySelector(`#icon-${className}`);
-        const coords=activeAnchor.getBoundingClientRect();
+function navCheck(entries) {
+    entries.forEach(entry => {
+        const className = entry.target.className;
+        const activeAnchor = document.querySelector(`#icon-${className}`);
+        const coords = activeAnchor.getBoundingClientRect();
 
-        if(entry.isIntersecting){
-            bar.style.left=`${coords.left+63}px`;
-            bar.style.top=`${coords.top-165}px`;
-            bar.style.width=`3px`;
-            bar.style.height=`70px`;
-            document.querySelector('#icon-video').src=`icon_video.svg`;
-            document.querySelector('#icon-stats').src=`icon_stats.svg`;
-            activeAnchor.src=`icon_${className}_focused.svg`;
+        if (entry.isIntersecting) {
+            setBounds(coords);
+            document.querySelector('#icon-video').src = `icon_video.svg`;
+            document.querySelector('#icon-stats').src = `icon_stats.svg`;
+            activeAnchor.src = `icon_${className}_focused.svg`;
         }
     });
 }
-sections.forEach(section =>{
+sections.forEach(section => {
     observer.observe(section)
 });
 
-let keyStopper=false;
-
-window.addEventListener('keydown', (e)=>{
-    if(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+window.addEventListener('keydown', (e) => {
+    if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
         e.preventDefault();
         // to prevent scrolling through arrow keys
     }
-    if(keyStopper){
-        setTimeout(function(){
-            console.log("Long Press "+e.code);
-        },1000);
-    }
-    else{
-        keyStopper=true;
-        console.log(e.code);
-    }
-    
 }, false);
-
-window.addEventListener('keyup',(e)=>{
-    keyStopper=false;
-})
-
-/* 0.25 keydown */
-/* keypress */
