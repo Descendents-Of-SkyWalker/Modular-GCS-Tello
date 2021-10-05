97ecaedb02d02276f06d4b53e620e32226a80ec7
Rajvi Jasani <rajvijasani1@gmail.com>
2021-10-05 22:15:56 +0530

    Video and Stats page frontend

--- frontend/main/script.js
+++ frontend/main/script.js
@@ -1,14 +1,120 @@
 const bar = document.querySelector('.bar');
 const links = document.querySelectorAll('a[href^="#"]');
 const sections = document.querySelectorAll('section');
+const buttonList = document.querySelectorAll('button');
 const navCoords = document.querySelector('nav').getBoundingClientRect();
 const startCoords = document.querySelector('#icon-video').getBoundingClientRect();
-setBounds(startCoords);
 
+setBounds(startCoords);
 function setBounds(coords) {
     bar.style.top = `${coords.y}px`;
     bar.style.height = `${coords.height}px`;
     bar.style.left = `${navCoords.width - 4}px`;
+}
+
+let currentLevel = 50;
+const innerCircle = document.querySelector('.inner-circle');
+innerCircle.addEventListener('click', () => {
+    const liquid = document.querySelector('.after');
+    liquid.style.animation = "none";
+    setTimeout(() => { liquid.style.animation = ""; }, 1)
+    const a = Math.floor(Math.random() * 100);
+    document.documentElement.style.setProperty('--initial-battery', `${currentLevel}%`);
+    document.documentElement.style.setProperty('--battery-level', `${100 - a}%`);
+    currentLevel = 100 - a;
+
+    document.querySelector('.before').textContent = `${a}%`;
+    document.querySelector('#level').textContent = `Remaining: ${a}%`;
+});
+
+// function updateBattery() {
+//     const liquid = document.querySelector('.after');
+//     liquid.style.animation = "none";
+//     setTimeout(() => { liquid.style.animation = ""; }, 1)
+//     const a = Math.floor(Math.random() * 100);
+//     document.documentElement.style.setProperty('--initial-battery', `${currentLevel}%`);
+//     document.documentElement.style.setProperty('--battery-level', `${100 - a}%`);
+//     currentLevel = 100 - a;
+
+//     document.querySelector('.before').textContent = `${a}%`;
+//     document.querySelector('#level').textContent = `Remaining: ${a}%`;
+// }
+// setInterval(updateBattery, 7000);
+
+
+let i = 0;
+var chartInterval;
+buttonList.forEach(btn => {
+    btn.addEventListener('click', () => {
+        clearInterval(chartInterval);
+        const graphDiv = document.querySelector('.graph');
+        graphDiv.removeChild(graphDiv.firstChild);
+        const canvas = document.createElement('canvas');
+        canvas.id = 'chartContainer';
+        graphDiv.appendChild(canvas);
+        let chart = addChart();
+        i = 0;
+        chartInterval = setInterval(updateChart, 1000, chart)
+    });
+});
+
+function updateChart(chart) {
+    if (i < 10) {
+        chart.data.labels.push(i.toString());
+        chart.data.datasets[0].data.push(Math.floor(Math.random() * 100));
+        i++;
+        // console.log(i);
+        chart.update();
+    }
+
+}
+function addChart() {
+
+    var ctx = document.querySelector('#chartContainer').getContext('2d');
+    Chart.defaults.font.size = 10;
+    Chart.defaults.plugins.title.display = false;
+    Chart.defaults.plugins.legend.display = false;
+
+    var gradientFill = ctx.createLinearGradient(0, 0, 0, 500);
+    gradientFill.addColorStop(0, "rgba(2, 119, 250, 0.2)");
+    gradientFill.addColorStop(0.5, "rgba(2, 119, 250, 0.1)");
+    gradientFill.addColorStop(1, "rgba(2, 119, 250, 0)");
+
+    var chart = new Chart(document.querySelector('#chartContainer'), {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [{
+                pointBackgroundColor: '#0277FA',
+                borderColor: '#0277FA',
+                data: [],
+                tension: 0.5,
+                fill: true,
+                backgroundColor: gradientFill,
+            }]
+        },
+        options: {
+            elements: {
+                point: {
+                    radius: 1
+                }
+            },
+            scales: {
+                y: {
+                    min: 0,
+                    grid: {
+                        display: false
+                    }
+                },
+                x: {
+                    grid: {
+                        display: false
+                    }
+                }
+            }
+        }
+    });
+    return chart;
 }
 
 links.forEach(link => {
